3a97eaa8edebdc9437872a1bba75f26d9839cee8
xingyue <1716265400@qq.com>
2020-03-20 14:16:06 +0800

    增加从txt和从数据库获得用例数据

--- src/test/java/LoginData/LoginParams.java
+++ src/test/java/LoginData/LoginParams.java
@@ -14,16 +14,16 @@ public class LoginParams {
      * 提供用户登陆测试方法数据
      * @return
      */
+   /* @DataProvider
+    public Object[][] getUsers(){
+        ReadFromFile read=new ReadFromFile();
+        return  read.createArray("E:/read1.txt");
+    }*/
+
     @DataProvider
     public Object[][] getUsers(){
-        return new Object[][]{
-                {"zhangsan","123456","欢迎zhangsan"},
-                {"lisi","","用户名或密码不能为空"},
-                {"","","用户名或密码不能为空"},
-                {"admin","123456","欢迎管理员"},
-                {"admin","","用户名或密码不能为空"},
-                {"","123456","用户名或密码不能为空"}
-        };
+        SelectSql read=new SelectSql();
+        return  read.result();
     }
 
 
--- src/test/java/TestXunit/LoginTest.java
+++ src/test/java/TestXunit/LoginTest.java
@@ -6,6 +6,9 @@ import LoginData.LoginParams;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  * Created by duzhe on 2018/12/23.
  *
@@ -13,11 +16,12 @@ import org.testng.annotations.Test;
  */
 public class LoginTest {
 
-    @Test(dataProvider = "getUsers", dataProviderClass = GenrateData.class)
+    @Test(dataProvider = "getUsers", dataProviderClass = LoginParams.class)
     public void testLogin(String name1, String pwd1, String expect1){
         Login login = new Login();
         String ac = login.userLogin(name1,pwd1);
-        Assert.assertEquals(ac,expect1);
+
+        assertThat("返回符合预期？",ac,equalTo(expect1));
     }
 
 }
